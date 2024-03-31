8b18fedc20a609c16ac3189bf133b18cd57c7fb4
Mark Grigorov <Grigorovmark7@gmail.com>
2024-03-31 16:53:37 +0300

    Update views.py

--- pfc_calculator/home/views.py
+++ pfc_calculator/home/views.py
@@ -5,3 +5,19 @@ def index(request):
 
 def about(request):
     return render(request, 'pfc_calculator/about.html')
+
+def result(request):
+    age = request.GET['age']
+    gender = request.GET['gender']
+    hight = request.GET['hight']
+    weight = request.GET['weight']
+
+    #data = {'age': age, 'gender': gender, 'hight': hight, 'weight': weight}
+    
+    if gender == 'male':
+        res = 10*int(hight)+6.25*int(weight)-5*int(age)+5
+
+    elif gender == 'female':
+        res = 10*int(hight)+6.25*int(weight)-5*int(age)-161
+
+    return render(request, 'pfc_calculator/result.html', {'res': res})
