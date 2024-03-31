4bfdf1578a73c4a151c291dee51742b87f09273f
Mark Grigorov <Grigorovmark7@gmail.com>
2024-03-31 20:54:38 +0300

    Update views.py

--- pfc_calculator/home/views.py
+++ pfc_calculator/home/views.py
@@ -20,4 +20,20 @@ def result(request):
     elif gender == 'female':
         res = 10*int(hight)+6.25*int(weight)-5*int(age)-161
 
-    return render(request, 'pfc_calculator/result.html', {'res': res})
+    else:
+        res = 0
+        imt = 0
+        imt_res = 'Вы что-то не указали'
+        return render(request, 'pfc_calculator/result.html', {'res': res, 'imt': imt, 'imt_res': imt_res})
+
+    hight = int(hight)/100
+    imt = int(weight)/(hight*hight)
+    if 0 < imt < 19:
+        imt_res = 'Дифицит массы тела'
+    elif 19.1 < imt < 25:
+        imt_res = 'Вес в норме'
+    
+    elif imt > 25.01:
+        imt_res = 'Избыточный вес'
+
+    return render(request, 'pfc_calculator/result.html', {'res': res, 'imt': imt, 'imt_res': imt_res})
