6f7c9af31a7a3732d44923f5e29ae75a4de69317
Mark Grigorov <Grigorovmark7@gmail.com>
2024-04-01 23:14:25 +0300

    Update urls.py

--- pfc_calculator/pfc_calculator/urls.py
+++ pfc_calculator/pfc_calculator/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from home import views
 
 urlpatterns = [
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('result/', views.result, name='result'),
+    path('users/', include('users.urls', namespace='users'))
 ]
